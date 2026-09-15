1930bf4688e98238010f8c288546949469aeeb44
matsushitaocr <matsushita.ocr@gmail.com>
2026-09-15 20:16:04 +0900

    Add files via upload

---
+++ secrets.js
@@ -0,0 +1,7 @@
+// このファイルは GitHub に上がりません（.gitignore）
+// GAS の CONFIG.API_TOKEN にも、下の API_TOKEN と「同じ文字」を入れてください
+
+window.RECEIPT_OCR = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyVGA7D32e0ROmZy3z_qn2O_DeYZzyggKQZpsYoH4Cvs2rhFjhOMXbXXkFtFfBQGOu2/exec',
+  API_TOKEN: 'KYnvjTzbBNbViDNMUjqZoksZhstH2JjKkbHgomuu'
+};
