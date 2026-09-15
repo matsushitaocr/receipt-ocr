021f5f44795dc775dd537b5b1427ae13880a11cb
matsushitaocr <matsushita.ocr@gmail.com>
2026-09-15 21:20:51 +0900

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -2,6 +2,6 @@
 // GAS の CONFIG.API_TOKEN にも、下の API_TOKEN と「同じ文字」を入れてください
 
 window.RECEIPT_OCR = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzvul2cvzuEA58xh5Ll8H6C_9otbbNS56iJmUKJmBl_gz0diuWaOCNTklKKyECZb17T/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzVL7YPD_iVh-qWHNQnL4xvwdgiY6m4s-k8xvbK9oGJIy3fByRJmXjkP2bPGsbq5UIK/exec',
   API_TOKEN: 'KYnvjTzbBNbViDNMUjqZoksZhstH2JjKkbHgomuu'
 };
